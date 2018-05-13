8b0bdfac4da248ee2211bbd2bfa236547e603659
Denakh <denakhokh@gmail.com>
2018-05-13 16:17:40 +0200

    D.13.05

--- src/main/java/mainpackage/entities/overallbalance/OverallBalance.java
+++ src/main/java/mainpackage/entities/overallbalance/OverallBalance.java
@@ -1,4 +1,19 @@
 package mainpackage.entities.overallbalance;
 
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
 public class OverallBalance {
+
+    //@Column(name = "msg_date")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date date;
+
+    private double factualBalance;
+
+    private double intriesBalance;
+
+
+
 }
--- src/main/java/mainpackage/entities/reserve/Reserve.java
+++ src/main/java/mainpackage/entities/reserve/Reserve.java
@@ -2,5 +2,18 @@ package mainpackage.entities.reserve;
 
 import mainpackage.entities.ItemOfExpenses;
 
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
 public class Reserve extends ItemOfExpenses {
+
+    //@Column(name = "msg_date")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date date;
+
+    private String description;
+
+    private double amount;
+
 }
