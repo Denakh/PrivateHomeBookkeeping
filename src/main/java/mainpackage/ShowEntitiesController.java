d4a82b9dc8ee40e7a482379c9fc76e6e4767aecc
Denakh <denakhokh@gmail.com>
2018-07-04 23:29:09 +0300

    D.04.07

---
+++ src/main/java/mainpackage/ShowEntitiesController.java
@@ -0,0 +1,85 @@
+package mainpackage;
+
+import mainpackage.entities.allocationofprofits.AllocationOfProfits;
+import mainpackage.entities.allocationofprofits.AllocationOfProfitsService;
+import mainpackage.entities.charity.Charity;
+import mainpackage.entities.charity.CharityService;
+import mainpackage.entities.currentexpensesrate.CurrentExpensesRate;
+import mainpackage.entities.currentexpensesrate.CurrentExpensesRateService;
+import mainpackage.entities.health.Health;
+import mainpackage.entities.health.HealthService;
+import mainpackage.entities.income.GeneralIncome;
+import mainpackage.entities.income.GeneralIncomeService;
+import mainpackage.entities.income.Income;
+import mainpackage.entities.income.IncomeService;
+import mainpackage.entities.kidsandpets.KidsAndPets;
+import mainpackage.entities.kidsandpets.KidsAndPetsService;
+import mainpackage.entities.othercapitaloutlays.OtherCapitalOutlays;
+import mainpackage.entities.othercapitaloutlays.OtherCapitalOutlaysService;
+import mainpackage.entities.recreation.Recreation;
+import mainpackage.entities.recreation.RecreationService;
+import mainpackage.entities.reserve.Reserve;
+import mainpackage.entities.reserve.ReserveService;
+import mainpackage.entities.users.CustomUser;
+import mainpackage.entities.users.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+@Controller
+public class ShowEntitiesController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private IncomeService incomeService;
+    @Autowired
+    private CharityService charityService;
+    @Autowired
+    private HealthService healthService;
+    @Autowired
+    private KidsAndPetsService kidsAndPetsService;
+    @Autowired
+    private OtherCapitalOutlaysService otherCapitalOutlaysService;
+    @Autowired
+    private RecreationService recreationService;
+    @Autowired
+    private ReserveService reserveService;
+    @Autowired
+    private GeneralIncomeService generalIncomeService;
+    @Autowired
+    private AllocationOfProfitsService allocationOfProfitsService;
+    @Autowired
+    private CurrentExpensesRateService currentExpensesRateService;
+
+    @RequestMapping("/data_getting")
+    public String dataGetting() {
+        return "data_getting";
+    }
+
+    @RequestMapping("/financial_analysis")
+    public String financialAnalysis() {
+        return "financial_analysis";
+    }
+
+    /*
+    @RequestMapping("/allocation_of_profits_show")
+    public String allocationOfProfitsShow() {
+        return "allocation_of_profits_table";
+    }
+
+    @RequestMapping("/communalpay_statistic_show")
+    public String communalPayStatisticShow() {
+        return "communalpay_statistic_table";
+    }
+    */
+
+}
