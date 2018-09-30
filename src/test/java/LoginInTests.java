2bc656708dab0a16fba36865c3790af8fa9e2dce
Denakh <denakhokh@gmail.com>
2018-09-30 12:38:05 +0200

    D.30.09

--- src/test/java/LoginInTests.java
+++ src/test/java/LoginInTests.java
@@ -24,22 +24,20 @@ public class LoginInTests {
     @Test
     public void verifyLoginInError() {
         String expectedErrorMessagePart = "Wrong login or password";
-        String actualErrorMessage;
         driver.findElement(By.cssSelector("input[type='submit']")).click();
         WebElement errorMessageP = driver.findElement(By.xpath("//form/p"));
-        actualErrorMessage = errorMessageP.getText();
+        String actualErrorMessage = errorMessageP.getText();
         Assert.assertTrue(actualErrorMessage.contains(expectedErrorMessagePart));
     }
 
     @Test
     public void verifyLoginInCorrect() {
         String expectedCorrectMessagePart = "Your login is: user";
-        String actualCorrectMessage;
         driver.findElement(By.name("j_login")).sendKeys(testUserName);
         driver.findElement(By.name("j_password")).sendKeys(testUserPass);
         driver.findElement(By.cssSelector("input[type='submit']")).click();
         WebElement mainCustomPage = driver.findElement(By.cssSelector("div[class='head']"));
-        actualCorrectMessage = mainCustomPage.getText();
+        String actualCorrectMessage = mainCustomPage.getText();
         Assert.assertTrue(actualCorrectMessage.contains(expectedCorrectMessagePart));
     }
 
