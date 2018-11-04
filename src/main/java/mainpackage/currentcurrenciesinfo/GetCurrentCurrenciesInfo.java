9a41b8d19366a62e7f901e26715d8f2be675f8fc
Denakh <denakhokh@gmail.com>
2018-11-04 14:29:25 +0200

    D.04.11

--- src/main/java/mainpackage/currentcurrenciesinfo/GetCurrentCurrenciesInfo.java
+++ src/main/java/mainpackage/currentcurrenciesinfo/GetCurrentCurrenciesInfo.java
@@ -1,6 +1,9 @@
 package mainpackage.currentcurrenciesinfo;
 
 import com.google.gson.Gson;
+import mainpackage.currentcurrenciesinfo.currenciesinfofinanceua.Cities;
+import mainpackage.currentcurrenciesinfo.currenciesinfofinanceua.CurrencyStatsFinanceUa;
+import mainpackage.currentcurrenciesinfo.currenciesinfofinanceua.Organization;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -8,17 +11,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class GetCurrentCurrenciesInfo {
 
-    public CurrencyCurrentStatNBU[] CurrencyCurrentStat() {
+    public CurrencyCurrentStatNBU[] CurrencyCurrentStatFromNBU() {
         Gson gson = new Gson();
         return gson.fromJson(getRespBodyByRestTemplate("https://bank.gov.ua",
                 "/NBUStatService/v1/statdirectory/exchange?json"), CurrencyCurrentStatNBU[].class);
     }
 
+    public CurrencyStatsFinanceUa CurrencyCurrentStatFromFinanceUa() {
+        Gson gson = new Gson();
+        return gson.fromJson(getRespBodyByRestTemplate("http://resources.finance.ua",
+                "/ru/public/currency-cash.json"), CurrencyStatsFinanceUa.class);
+    }
 
-    private String getRespBodyByRestTemplate(String server, String uri) {
+    private static String getRespBodyByRestTemplate(String server, String uri) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
         headers.add("Accept", "text/*");
@@ -28,25 +39,26 @@ public class GetCurrentCurrenciesInfo {
         return responseEntity.getBody();
     }
 
-/*
+
     public static void main(String[] args) {
         Gson gson = new Gson();
-        Currency[] array = gson.fromJson(getRespBodyByRestTemplate("https://bank.gov.ua",
-                "/NBUStatService/v1/statdirectory/exchange?json"), Currency[].class);
-        System.out.println(array.length);
-        for (Currency currency : array) {
-            switch (currency.getTxt()) {
-                case "Долар США":
-                    System.out.println("USD: " + currency.getRate());
-                    break;
-                case "Євро":
-                    System.out.println("EUR: " + currency.getRate());
-                    break;
-            }
+        CurrencyStatsFinanceUa currencyStatsFinanceUa = gson.
+                fromJson(getRespBodyByRestTemplate("http://resources.finance.ua",
+                "/ru/public/currency-cash.json"), CurrencyStatsFinanceUa.class);
+
+        List<Organization> organizationList = currencyStatsFinanceUa.getOrganizations();
+
+        List<Double> ratesListUSD = new ArrayList<>();
+        List<Double> ratesListEUR = new ArrayList<>();
+
+
+        for (Organization organization : organizationList) {
+            if (organization.getCurrencies().getUSD() != null)
+            System.out.println(organization.getCurrencies().getUSD().getBid());
         }
 
     }
-    */
+
 /*
     private static String getByRestAssured() {
         return RestAssured.
--- src/main/java/mainpackage/currentcurrenciesinfo/currenciesinfofinanceua/CurrencyStatsFinanceUa.java
+++ src/main/java/mainpackage/currentcurrenciesinfo/currenciesinfofinanceua/CurrencyStatsFinanceUa.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-public class CurrencyStats {
+public class CurrencyStatsFinanceUa {
 
     @SerializedName("sourceId")
     @Expose
